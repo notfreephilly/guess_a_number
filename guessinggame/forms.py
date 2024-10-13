16b39fee26629c05208ef6dcd6cf34a65b361944
notfreephilly <not.philyoder93@gmail.com>
2024-10-13 12:12:57 -0700

    installed crispy template, created a register.html page, forms.py file so users can create accounts

--- guessinggame/forms.py
+++ guessinggame/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
+"""
+This form is a custom user registration form that collects a username,
+email, password.
+
+Ensures email is required and saves the new user in the database
+
+The 'save' method is customized to handle setting the email field before saving the user
+"""
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
