669aa1b7ee721d70bd0caf054c1bfc02d821cb3a
notfreephilly <not.philyoder93@gmail.com>
2024-10-13 15:24:28 -0700

    finished authentication, registration form, login, profile features

--- guessinggame/urls.py
+++ guessinggame/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('play', views.number_guessing_game, name='play'),
-    path('register', views.register_request, name='register'),
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('profile/', views.profile, name='profile'),
 ]
